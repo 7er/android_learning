fa3d513bc5dda1ddcfdbf827d63f2e937f2f0f7b
Syver Enstad <syver.enstad@cisco.com>
2012-02-16 10:07:43 +0100

    Add interfaces for framework

---
+++ GameFramework/src/com/androidgames/framework/GameFrameworkActivity.java
@@ -0,0 +1,13 @@
+package com.androidgames.framework;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameFrameworkActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
